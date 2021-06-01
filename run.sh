d0f2ac7da310ad70031bdf538d27dfd2e1568938
Agustina Aldasoro <agusaldasoro@gmail.com>
2021-06-01 11:44:05 -0300

    Improve message in run script

--- run.sh
+++ run.sh
@@ -8,6 +8,6 @@ docker run -d -p 5000:5000 $DOCKER_IMAGE_NAME
 
 DOCKER_ID=$(docker ps | grep $DOCKER_IMAGE_NAME | awk '{ print $1 }')
 
-echo "Running docker image: $DOCKER_ID"
+echo "Running docker image: '$DOCKER_IMAGE_NAME' with id:  '$DOCKER_ID'"
 
-# docker exec -it ${DOCKER_ID} /bin/sh
+# docker exec -it ${DOCKER_IMAGE_NAME} /bin/sh
