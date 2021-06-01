25d54ab46dcdb4bca741f779ff83a0bb598591f8
Agustina Aldasoro <agusaldasoro@gmail.com>
2021-06-01 11:41:39 -0300

    Save public key to file

--- src/components.ts
+++ src/components.ts
@@ -2,6 +2,7 @@ import { createDotEnvConfigComponent } from '@well-known-components/env-config-p
 import { createServerComponent, createStatusCheckComponent } from '@well-known-components/http-server'
 import { createLogComponent } from '@well-known-components/logger'
 import { createMetricsComponent } from '@well-known-components/metrics'
+import * as fs from 'fs'
 import { generateSigningKeys } from './logic/key-generator'
 import { metricDeclarations } from './metrics'
 import { createFetchComponent } from './ports/fetch'
@@ -9,6 +10,7 @@ import { AppComponents, GlobalContext } from './types'
 
 // Initialize all the components of the app
 export async function initComponents(): Promise<AppComponents> {
+  console.error('QUEE')
   const config = await createDotEnvConfigComponent({})
   const logs = createLogComponent()
   const server = await createServerComponent<GlobalContext>({ config, logs }, {})
@@ -16,6 +18,21 @@ export async function initComponents(): Promise<AppComponents> {
   const fetch = await createFetchComponent()
   const metrics = await createMetricsComponent(metricDeclarations, { server, config })
   const keys = generateSigningKeys()
+
+  const dir = 'secrets'
+
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir)
+  }
+
+  if (fs.existsSync(`${dir}/public_key.pem`)) {
+    fs.unlinkSync(`${dir}/public_key.pem`)
+  }
+  // Store to file the public key
+  fs.writeFile(`${dir}/public_key.pem`, keys.publicKey, (err) => {
+    // throws an error, you could also catch it here
+    if (err) throw err
+  })
 
   return {
     config,
