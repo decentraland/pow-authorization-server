12539f5922fdea301e5bc32d2b13a21420b09a23
Agustina Aldasoro <agusaldasoro@gmail.com>
2021-07-26 09:40:04 -0300

    Add path to set cookie

--- src/logic/cookie.ts
+++ src/logic/cookie.ts
@@ -1,5 +1,10 @@
 import * as cookie from 'cookie'
 
 export function getCookieHeader(signedJWT: string) {
-  return { 'Set-Cookie': cookie.serialize('JWT', signedJWT, { expires: new Date(Date.now() + 7 * 24 * 3600 * 1000) }) }
+  return {
+    'Set-Cookie': cookie.serialize('JWT', signedJWT, {
+      expires: new Date(Date.now() + 7 * 24 * 3600 * 1000),
+      path: '/'
+    })
+  }
 }
